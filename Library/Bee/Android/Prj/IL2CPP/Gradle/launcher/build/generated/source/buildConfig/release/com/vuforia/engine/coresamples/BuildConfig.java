8d0acfa1e542c2df03fc89d8ead522dc20abf904
Xuluocai <cxl1815093870@gmail.com>
2022-11-26 16:39:20 +0100

    1

---
+++ Library/Bee/Android/Prj/IL2CPP/Gradle/launcher/build/generated/source/buildConfig/release/com/vuforia/engine/coresamples/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vuforia.engine.coresamples;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.vuforia.engine.coresamples";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1093;
+  public static final String VERSION_NAME = "10.9.3";
+}
